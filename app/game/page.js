0a47eb7bd64d061e335e1addb12717ae0bb08b95
Guney Unay <guneymarketing17@hotmail.com>
2023-12-30 17:09:36 +0300

    added game container

--- app/game/page.js
+++ app/game/page.js
@@ -1,15 +1,27 @@
 "use client";
 import React from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import GameContainer from "@/containers/GameContainer";
 function Game() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const searchs = searchParams.has("theme" && "players" && "gridSize");
+  // PARAMS
+  const theme = searchParams.get("theme");
+  const players = searchParams.get("players");
+  const gridSize = searchParams.get("gridSize");
 
   if (!searchs) {
     router.push("/");
   }
-  return <div>game</div>;
+
+  return (
+    <GameContainer
+      THEME={theme}
+      NUMBERS_OF_PLAYERS={players}
+      GRID_SIZE={gridSize}
+    />
+  );
 }
 
 export default Game;
---
+++ containers/GameContainer/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GameContainer({ THEME, NUMBERS_OF_PLAYERS, GRID_SIZE }) {
+  return <main className="bg-[#fcfcfc]"></main>;
+}
+
+export default GameContainer;
