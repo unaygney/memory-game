a766f7bc33a587c94f13ae28b8aade71076f7de5
Guney Unay <guneymarketing17@hotmail.com>
2023-12-31 19:30:39 +0300

    added rotating style

--- containers/GameContainer/GameBoard/index.jsx
+++ containers/GameContainer/GameBoard/index.jsx
@@ -6,6 +6,36 @@ import { generateBoard } from "@/lib/generetaBoard";
 function GameBoard({ GRID_SIZE }) {
   const SIZE = GRID_SIZE === "6x6" ? 6 : 4;
   const [board, setBoard] = useState(generateBoard(SIZE));
+  const [selectedCells, setSelectedCells] = useState([]);
+  const handleClick = (rowIdx, cellIdx) => {
+    const newSelectedCells = [...selectedCells, { row: rowIdx, cell: cellIdx }];
+
+    if (newSelectedCells.length === 2) {
+      const [firstCell, secondCell] = newSelectedCells;
+      const [row1, cell1] = [firstCell.row, firstCell.cell];
+      const [row2, cell2] = [secondCell.row, secondCell.cell];
+
+      if (board[row1][cell1] === board[row2][cell2]) {
+        // Eşleşen butonları burada işaretleyebilirsiniz.
+        const updatedBoard = [...board];
+        updatedBoard[row1][cell1] = "matched";
+        updatedBoard[row2][cell2] = "matched";
+        setBoard(updatedBoard);
+      } else {
+        // Eşleşmeyen butonları işaretleyebilirsiniz (Örn: Kırmızıya çevirme).
+        // Örneğin, eşleşmeyen butonları bir süre sonra geri çevirebilirsiniz.
+        setTimeout(() => {
+          const updatedBoard = [...board];
+          updatedBoard[row1][cell1] = "unmatched";
+          updatedBoard[row2][cell2] = "unmatched";
+          setBoard(updatedBoard);
+        }, 1000);
+      }
+      setSelectedCells([]);
+    } else {
+      setSelectedCells(newSelectedCells);
+    }
+  };
 
   return (
     <section className={`w-full flex justify-center`}>
@@ -23,10 +53,16 @@ function GameBoard({ GRID_SIZE }) {
               <button
                 className={` inline-flex ${
                   SIZE === 6 ? "h-[46px] md:h-[82px]" : "h-[72px] md:h-[118px]"
-                }  items-center justify-center rounded-full bg-[#BCCED9]`}
+                }  items-center justify-center rounded-full   card  `}
                 key={i}
+                onClick={() => handleClick(idx, i)}
               >
-                <span className="text-[#fcfcfc]  heading-l">{CELL}</span>
+                <div className="card-side front w-full h-full ">
+                  <span className="absolute inset-0 bg-[#304859] rounded-full w-full h-full "></span>
+                </div>
+                <div className="card-side back flex items-center justify-center ">
+                  <span className="text-[#fcfcfc]  heading-l">{CELL}</span>
+                </div>
               </button>
             ))}
           </div>
--- containers/GameContainer/Header/index.jsx
+++ containers/GameContainer/Header/index.jsx
@@ -5,7 +5,7 @@ function Header() {
   const [isActive, setActive] = useState(false);
   return (
     <>
-      <header className=" bg-transparent md:pt-10 lg:pt-[67px]  ">
+      <header className=" bg-transparent md:pt-10 lg:pt-[32px]  ">
         <div className="container mx-auto">
           <div className="flex justify-between">
             <Link
